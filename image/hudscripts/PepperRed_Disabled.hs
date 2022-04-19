c957fc48e36e38fe5df9a760615f259c99836175
Rainchus <Rainchus@github.com>
2022-04-19 09:42:46 -0500

    fully implement peppers

--- image/hudscripts/PepperRed_Disabled.hs
+++ image/hudscripts/PepperRed_Disabled.hs
@@ -1,6 +1,6 @@
 SetVisible
 SetTileSize     ( .IconSize:32x32 )
 Loop
-	SetIcon         ( 60` ~ImageIcon:item/battle/pepperblue )
+	SetIcon         ( 60` ~ImageIcon:item/battle/pepperred_alt1 )
 Restart
 End
